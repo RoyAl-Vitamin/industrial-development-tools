b3e4108e3d92711f6234334ed9e9b4a0e2b36d98
RoyAl-Vitamin <roalvi95@gmail.com>
2018-11-22 16:07:29 +0500

    Настроил CORS онфигурацию

--- src/main/java/vi/al/ro/api/UserApi.java
+++ src/main/java/vi/al/ro/api/UserApi.java
@@ -45,12 +45,7 @@ public class UserApi {
         } catch (NullPointerException npe) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .headers(headers)
-                .body(users);
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     /**
